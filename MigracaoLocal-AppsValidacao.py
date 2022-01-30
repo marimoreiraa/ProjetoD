897b36d0334077b34cbba8b478bb1eb7711a2f88
Edlaine Sá <c.edlainesa@gmail.com>
2022-01-30 17:43:18 -0300

    Add files via upload
    Adição de scripts de migração:
    MigracaoLocal-AppsTreino.py
    MigracaoLocal-AppsValidacao.py
    MigracaoLocalNuvem-Apps.py

---
+++ MigracaoLocal-AppsValidacao.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import psycopg2
+import csv
+
+
+arquivos = []
+df_apps_validacao = pd.read_csv(
+    'entrada_APPS_Apps_Validacao.csv',
+    sep=";",
+    encoding='utf-8')
+
+# print(df_apps_validacao)
+
+list_apps_validacao = df_apps_validacao.values.tolist()
+
+# print(list_apps_validacao[:10])
+
+class connector_postgres:
+    host = "localhost"
+    database = "TabelasFinais"
+    user = "postgres"
+    password = "deg2205"
+
+    def conectar(self):
+        conn = psycopg2.connect(host=self.host,
+                                database=self.database,
+                                user=self.user,
+                                password=self.password)
+        cursor = conn.cursor()
+
+        return conn, cursor
+
+    def desconectar(self, conn, cursor):
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+    def inserir(self, query):
+        conn, cursor = self.conectar()
+        cursor.execute(query)
+        self.desconectar(conn, cursor)
+
+    def selecionar(self, query):
+        conn, cursor = self.conectar()
+        cursor.execute(query)
+        lista = []
+        for result in cursor.fetchall():
+            lista.append(result)
+        self.desconectar(conn, cursor)
+        return lista
+
+    def update(self, query):
+        conn, cursor = self.conectar()
+        cursor.execute(query)
+        self.desconectar(conn, cursor)
+
+    def delete(self, query):
+        conn, cursor = self.conectar()
+        cursor.execute(query)
+        self.desconectar(conn, cursor)
+
+
+conect = connector_postgres()
+
+for i in list_apps_validacao:
+    query = "INSERT INTO entrada_apps_apps_validacao (instituicao, comentario, dataa, classificacao, elogio_quanto_ao_app, reclamacao_quanto_ao_app, elogio_a_instituicao, reclamacao_a_instituicao, nao_classificavel) VALUES('{}','{}','{}',{},'{}','{}','{}','{}','{}')".format(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8])
+
+    conect.inserir(query)
---
+++ MigracaoLocalNuvem-Apps.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import psycopg2
+import csv
+
+
+conn = psycopg2.connect(host="localhost",
+                        database="TabelasFinais",
+                        user="postgres",
+                        password="deg2205")
+
+cursor = conn.cursor()
+
+query1 = "SELECT * FROM entrada_apps_apps_treino_tratado"
+cursor.execute(query1)
+lista1=[]
+for result in cursor.fetchall():
+    lista1.append(result)
+
+query2 = "SELECT * FROM entrada_apps_apps_validacao_tratado"
+cursor.execute(query2)
+lista2=[]
+for result in cursor.fetchall():
+    lista2.append(result)
+    
+
+conn.commit()
+conn.close()
+
+
+
+
+class connector_postgres:
+    host = "34.151.204.5"
+    database = "apps"
+    user = "postgres"
+    password = "secret"
+
+    def conectar(self):
+        conn = psycopg2.connect(host=self.host,
+                                database=self.database,
+                                user=self.user,
+                                password=self.password)
+        cursor = conn.cursor()
+
+        return conn, cursor
+
+    def desconectar(self, conn, cursor):
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+    def inserir(self, query):
+        conn, cursor = self.conectar()
+        cursor.execute(query)
+        self.desconectar(conn, cursor)
+
+
+
+
+conect = connector_postgres()
+
+
+for i in lista1:
+    query1 = "INSERT INTO entrada_apps_apps_treino_tratado (instituicao, comentario, dataa, classificacao, elogio_quanto_ao_app, reclamacao_quanto_ao_app, elogio_a_instituicao, reclamacao_a_instituicao, nao_classificavel) VALUES('{}','{}','{}',{},'{}','{}','{}','{}','{}')".format(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8])
+
+    conect.inserir(query1)
+
+for i in lista2:
+    query2 = "INSERT INTO entrada_apps_apps_validacao_tratado (instituicao, comentario, dataa, classificacao, elogio_quanto_ao_app, reclamacao_quanto_ao_app, elogio_a_instituicao, reclamacao_a_instituicao, nao_classificavel) VALUES('{}','{}','{}',{},'{}','{}','{}','{}','{}')".format(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8])
+
+    conect.inserir(query2)
